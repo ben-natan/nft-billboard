b7ea871aa21ed19f7046d210d53e57cab6cdf74d
ben-natan <theadrien2008@gmail.com>
2023-11-15 16:51:17 -0500

    Downgraded ethers version

--- src/MintModal.js
+++ src/MintModal.js
@@ -1,6 +1,6 @@
 import contract from './contracts/nft-billboard.json';
-import { contractAddress } from "./App";
-import { ethers } from 'ethers';
+// import { contractAddress } from "./App";
+// import { ethers } from 'ethers';
 
 const abi = contract.abi;
 
@@ -8,36 +8,36 @@ export default function MintModal(props) {
     const { cellNumber } = props;
 
     // TODO: this is just an example
-    const mintHandler = async () => {
-        try {
-            const { ethereum } = window;
+    // const mintHandler = async () => {
+    //     try {
+    //         const { ethereum } = window;
 
-            if (ethereum) {
-                // TODO: move this out of func to make it global if needed more than once
-                const provider = new ethers.BrowserProvider(ethereum);
-                const signer = await provider.getSigner();
-                const billboardContract = new ethers.Contract(contractAddress, abi, signer);
+    //         if (ethereum) {
+    //             // TODO: move this out of func to make it global if needed more than once
+    //             const provider = new ethers.BrowserProvider(ethereum);
+    //             const signer = await provider.getSigner();
+    //             const billboardContract = new ethers.Contract(contractAddress, abi, signer);
 
-                let tx = await billboardContract.mint(); // TODO: implement mint() in the contract
+    //             let tx = await billboardContract.mint(); // TODO: implement mint() in the contract
 
-                await tx.wait();
+    //             await tx.wait();
 
-                console.log("Mined transaction: ", tx.hash);
-            } else {
-                console.error("Ethereum object does not exist");
-            }
-        } catch (err) {
-            console.error(err);
-        }
-    }
+    //             console.log("Mined transaction: ", tx.hash);
+    //         } else {
+    //             console.error("Ethereum object does not exist");
+    //         }
+    //     } catch (err) {
+    //         console.error(err);
+    //     }
+    // }
 
-    const MintButton = () => {
-        return (
-            <button onClick={mintHandler}>
-                Mint
-            </button>
-        )
-    }
+    // const MintButton = () => {
+    //     return (
+    //         <button onClick={mintHandler}>
+    //             Mint
+    //         </button>
+    //     )
+    // }
 
     return (
         <div style={{display: 'flex', flexDirection: 'column',
@@ -46,7 +46,8 @@ export default function MintModal(props) {
             {/* TODO: form to call the mint function */}
             <p>{cellNumber}</p>
             <div>
-                <MintButton />
+                <p> Here is the button </p>
+                {/* <MintButton /> */}
             </div>
         </div>
     )
