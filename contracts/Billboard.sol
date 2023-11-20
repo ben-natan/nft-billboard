88c9147b07e24eb5e099f53f142607fccd2e41b4
Abram Kremer <a.kremer@columbia.edu>
2023-11-20 16:00:22 -0500

    Update Billboard.sol
    Now stores coordinates alongside tokens. Prevent collisions and invalid coordinate entry. Also introduces a function to allow for updating art once a token is minted.

--- contracts/Billboard.sol
+++ contracts/Billboard.sol
@@ -1,57 +1,70 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts@5.0.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts@5.0.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@5.0.0/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Billboard is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract Billboard is ERC721, Ownable {
+
+    struct Tuple {
+        uint256 leftX;
+        uint256 rightX;
+        uint256 topY;
+        uint256 bottomY;
+    }
+
+    uint256 constant gridSize = 1000;
     uint256 private _nextTokenId;
+    bool[gridSize*gridSize] public coordTaken;
+    mapping(uint256 => Tuple) public idToCoordinates;
+    mapping(uint256 => uint256[]) public idToArt;
 
     constructor(address initialOwner)
         ERC721("Billboard", "BILLBOARD")
         Ownable(initialOwner)
     {}
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, uint256 leftX, uint256 rightX, uint256 topY, uint256 bottomY) public {
+        
+        // ensuring valid coordinates
+        require(leftX <= rightX && topY <= bottomY, "Invalid Coordinates: Backwards x ordering");
+        require(leftX >= 0 && topY >= 0, "Invalid Coordinates: Must be >= 0");
+        require(bottomY <= gridSize && rightX <= gridSize, "Invalid Coordinates: Must be <= GridSize");
+        
+        // ensures no collisions with previously minted tokens and also stores new coords on collision map
+        Tuple memory coords = Tuple(leftX, rightX, topY, bottomY);
+        checkCollisions(coords);
+
         uint256 tokenId = _nextTokenId++;
+        idToCoordinates[tokenId] = coords;
+
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
     }
 
-    // The following functions are overrides required by Solidity.
+    function updateArt(uint256 tokenId, uint256[] calldata art) public {
+        
+        require(msg.sender == ownerOf(tokenId));
+        Tuple memory coords = idToCoordinates[tokenId];
+        require(art.length == (coords.rightX - coords.leftX + 1)*(coords.bottomY - coords.topY + 1));
 
-    function _update(address to, uint256 tokenId, address auth)
-        internal
-        override(ERC721, ERC721Enumerable)
-        returns (address)
-    {
-        return super._update(to, tokenId, auth);
+        idToArt[tokenId] = art;
     }
 
-    function _increaseBalance(address account, uint128 value)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._increaseBalance(account, value);
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
+    function checkCollisions(Tuple memory coords) internal {
+        
+        for (uint256 x = coords.leftX; x <= coords.rightX; x++) {
+            for (uint256 y = coords.topY; y <= coords.bottomY; y++) {
+
+                require(!coordTaken[x + y*gridSize], "Invalid Coordinates: Coordinates unavailable");
+
+                coordTaken[x + y*gridSize] = true;
+
+            }
+        }
     }
 
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable, ERC721URIStorage)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
 }
