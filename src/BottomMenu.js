7f01f6fda86d38cb744ce5cfb74a3d62de4c853b
Carla Morral Martin <31885862+carlaMorral@users.noreply.github.com>
2023-12-03 22:44:12 -0500

    bottom menu styled

--- src/BottomMenu.js
+++ src/BottomMenu.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { SELECTION_STATES, CELL_WIDTH, CELL_HEIGHT, colors } from "./Billboard";
 
-const BOTTOM_MENU_WIDTH = 300 // px
+const BOTTOM_MENU_WIDTH = 270 // px
 const BOTTOM_MENU_HEIGHT = 80 // px
 
 export default function BottomMenu(props) {
@@ -24,14 +24,18 @@ export default function BottomMenu(props) {
 
     return (
         <div style={{
-            width: BOTTOM_MENU_WIDTH + "px",
-            height: BOTTOM_MENU_HEIGHT + "px",
+            height: BOTTOM_MENU_HEIGHT + 'px',
+            width: BOTTOM_MENU_WIDTH + 'px',
             position: 'fixed',
-            backgroundColor: 'lightblue',
+            backgroundColor: '#282c34',
             top: '85vh',
             display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center'
+            flexDirection: 'column',
+            alignItems: 'center',
+            color: 'white',
+            padding: '12px',
+            alignItems: 'center',
+            justifyContent: 'center'
             }}
 
             onMouseEnter={onMouseEnter}
@@ -39,12 +43,12 @@ export default function BottomMenu(props) {
             >
             {
                 selectionState == SELECTION_STATES.None &&
-                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                    <p style={{marginBottom: 0}}>Select an area to mint</p>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', lineHeight: 1.5, cursor: 'default'}}>
+                    <p style={{margin: 0, fontWeight: 'bold'}}>Select an area to mint</p>
                     {ownNFTs.length > -1 &&
-                        <p>or select&nbsp;
+                        <p style={{margin: 0}}>or select&nbsp;
                             <a onMouseEnter={() => outlineMultipleNFTs(ownNFTs)} onMouseLeave={clearOutlineNFTs}
-                                style={{textDecoration: 'underline'}}
+                                style={{color: 'lightblue'}}
                             >one of your areas
                             </a> to draw</p>
                     }
@@ -53,33 +57,65 @@ export default function BottomMenu(props) {
             {
                 selectionState == SELECTION_STATES.PickedOwnNFT &&
                 <>
-                    <p> Picked NFT: x = {pickedOwnNFT.startX}, y = {pickedOwnNFT.startY} </p>
-                    <button onClick={onClickDraw}> Draw! </button>
+                    <p> Picked NFT: x = {pickedOwnNFT.startX}, y = {pickedOwnNFT.startY}</p>
+                    <button onClick={onClickDraw} style={{
+                        background: 'lightblue',
+                        border: 'none',
+                        width: '60px',
+                        height: '30px',
+                        cursor: 'pointer'
+                    }}> Draw! </button>
                 </>
             }
             {
                 (selectionState == SELECTION_STATES.Selecting || selectionState == SELECTION_STATES.Done) &&  (
                 <>
-                    <button onClick={onMint}> mint </button>
-                    <p>{Math.abs(selectionCoords.width) / CELL_WIDTH} x {Math.abs(selectionCoords.height) / CELL_HEIGHT}</p>
-                    <button onClick={onClearSelection}>Clear</button>
+                    <button onClick={onMint} style={{
+                            background: 'lightgreen',
+                            border: 'none',
+                            width: '60px',
+                            height: '30px',
+                            cursor: 'pointer'
+                        }}>Mint</button>
+                    <p style={{margin: '5px'}}>{Math.abs(selectionCoords.width) / CELL_WIDTH} x {Math.abs(selectionCoords.height) / CELL_HEIGHT}</p>
+                    <button onClick={onClearSelection} style={{
+                            background: '#FFCCCB',
+                            border: 'none',
+                            width: '60px',
+                            height: '30px',
+                            cursor: 'pointer'
+                        }}>Clear</button>
                 </>
                 )
             }
             {
                 selectionState == SELECTION_STATES.Drawing &&
                 <div style={{display: 'flex', flexDirection: 'column'}}>
-                    <div style={{display: 'grid', gridTemplateColumns: 'auto auto auto auto auto auto auto auto'}}>
+                    <div style={{display: 'grid', cursor: 'pointer', gridTemplateColumns: 'auto auto auto auto auto auto auto auto'}}>
                         {colors.map((c, i) => (
                             <div
                                 key={i}
                                 style={{height: '30px', width: '30px', backgroundColor: c}}
                                 onClick={() => onPickColor(i)}
-                            >1</div>
+                            ></div>
                         ))}
                     </div>
-                    <button onClick={onCancelDraw}>Cancel</button>
-                    <button onClick={onSubmitDraw}>Submit</button>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                        <button onClick={onCancelDraw} style={{
+                            background: '#FFCCCB',
+                            border: 'none',
+                            width: '60px',
+                            height: '25px',
+                            cursor: 'pointer',
+                        }}>Cancel</button>
+                        <button onClick={onSubmitDraw} style={{
+                            background: 'lightgreen',
+                            border: 'none',
+                            width: '60px',
+                            height: '25px',
+                            cursor: 'pointer'
+                        }}>Submit</button>
+                    </div>
                 </div>
             }
 
